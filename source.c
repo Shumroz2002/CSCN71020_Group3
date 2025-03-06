ed7025610f52f13761718d8a2fc8e169008f4afe
Shumroz Usmani <shrezorhere2002@gmail.com>
2025-03-06 15:09:51 -0500

    We added the input interface for the sides for the triangle

--- source.c
+++ source.c
@@ -1,8 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <math.h>
-#include <stdlib.h>
 
 void get_triangle_input() {
+    
+    double side1, side2, side3;
+            
+    // FISRT SIDE USER INPUT
+    printf("Please Enter the first side\n");
+        // First Input Validation
+    int ScanReturn1 =  scanf("%lf", &side1);
+
+    // Second Side user input
+    printf("Please Enter The Second Side\n");
+        // Second Input Validation
+   int ScanReturn2 = scanf("%lf", &side2);
+
+
+    // Third Side User input
+    printf("Please Enter The Third Side\n");
+        // Third Side Validation
+    int ScanReturn3 = scanf("%lf", &side3);
+
 }
 
 void get_rectangle_input() {
