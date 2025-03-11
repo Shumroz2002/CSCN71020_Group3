9d33e57baabe86b0581d2f1b24f257d45393ddb0
Darshitaa27 <Dpatel6206@conestogac.on.ca>
2025-03-11 16:17:44 -0400

    Added Rectangle Function

--- source.c
+++ source.c
@@ -1,7 +1,57 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <math.h>
 
 void get_triangle_input() {
+<<<<<<< HEAD
+=======
+// VARIABLE DECLARATION FOR USER INPUT
+    double side1, side2, side3;
+     
+	// FIRST SIDE USER INPUT
+    do {
+        printf("Please Enter the first side: ");
+        ScanReturn1 = scanf("%lf", &side1);
+
+        while (getchar() != '\n');  // CLEARING INPUT BUFFER
+
+    } 
+    
+    while (ScanReturn1 != 1 || side1 <= 0);
+
+    // SECOND SIDE USER INPUT
+    do {
+        printf("Please Enter the second side: ");
+        ScanReturn2 = scanf("%lf", &side2);
+
+		while (getchar() != '\n');  // CLEARING INPUT BUFFER
+
+    } 
+    while (ScanReturn2 != 1 || side2 <= 0);
+
+    // THIRD SIDE USER INPUT
+    do {
+        printf("Please Enter the third side: ");
+        ScanReturn3 = scanf("%lf", &side3);
+
+		while (getchar() != '\n');  //  CLEARING INPUT BUFFER
+
+    } 
+    while (ScanReturn3 != 1 || side3 <= 0);
+
+    // Triangle Validity Check
+	if (side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1) { // INVALID TRIANGLE VALIDITY CHECK
+
+        printf("Invalid triangle sides. Please enter again.\n");
+
+		get_triangle_input(); // Restart THE FUNCTION
+
+        return;
+    }
+
+	printf("Valid Triangle Entered: %.2f, %.2f, %.2f\n", side1, side2, side3); // PRINTING VALID TRIANGLE SIDES
+}
+>>>>>>> dc5b7befa51fd18f71ff3a9221f7a55fd073482f
 
 }
 
